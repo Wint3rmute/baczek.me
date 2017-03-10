7a70c8e9f6c308b286e96e6e801bb49eaff48cb0
Mateusz Baczek <mateusz.baczek1998@gmail.com>
2017-03-10 20:28:39 +0100

    this is going on quite nicely

--- materialize/js/script.js
+++ materialize/js/script.js
@@ -1,16 +1,149 @@
-function getUsers()
-{
+$(document).ready(function() { getCategories(); });
 
-  var userId = "asd";
-  return firebase.database().ref('/users/').once('value').then(function(snapshot) {
-    Materialize.toast('Data Loaded!', 1000);
+
+var theCategory = "";
+
+function getCategories()
+{
+  theCategory = "";
+
+  var htmlPart1 = '<a onclick=chooseCategory("';
+  var htmlPart2 ='") class="collection-item">';
+  var htmlPart3 = '</a>'
+
+$( "#categoryList" ).empty();
+$("#questionsList").empty();
+$( "#categoryDiv" ).fadeOut(0);
+$("#questionsDiv").fadeOut(0);
+$("#newQuestionDiv").fadeOut(0);
+
+  return firebase.database().ref('/quizy/').once('value').then(function(snapshot) {
+    Materialize.toast('Data Loaded!', 800);
+
+    $("#loaderDiv").fadeOut(1000, function(){
+      $( "#categoryDiv" ).fadeIn(300);
+    });
+
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
-      var childData = childSnapshot.val();
-      $( "#articleList" ).append( '<li> <div class="collapsible-header">' + childKey + '</div> <div class="collapsible-body"><span>' + childData +  '</span></div> </li>');
+      //alert(childKey);
+      $("#categoryList").append(htmlPart1 + childKey + htmlPart2 + childKey + htmlPart3);
+
+
+
+      //$( "#articleList" ).append( '<li> <div class="collapsible-header">' + childKey + '</div> <div class="collapsible-body"><span>' + childData +  '</span></div> </li>');
 
     });
 
   });
+}
+
+function chooseCategory(category)
+{
+  $( "#categoryList" ).empty();
+  $("#questionsList").empty();
+  theCategory = category;
+
+    var html1 = '<li><div class="collapsible-header">';
+    //question here
+    var html2 = '</div><div class="collapsible-body"><ul class="collection"><li class="collection-item">';
+    //answer 1 here
+    var html3 = '</li><li class="collection-item">';
+    //answer 2 here;
+    //html3 again
+    //answer 3 here;
+    var html4 = '</li></ul></div></li>';
+    //and we're done!
+
+  $( "#categoryDiv" ).fadeOut(500, function()
+  {
+
+    return firebase.database().ref('/quizy/' + category).once('value').then(function(snapshot) {
+      Materialize.toast('Data Loaded!', 800);
+
+      snapshot.forEach(function(childSnapshot) {
+
+          var q;
+          var a1;
+          var a2;
+          var a3;
+        childSnapshot.forEach(function(lastSnapshot){
+
+          var childKey = lastSnapshot.key;
+          var childValue = lastSnapshot.val();
+
+          if(childKey == "question" )
+          {
+            q = childValue;
+          }else if (childKey == "a1")
+          {
+            a1 = childValue;
+          }
+          else if (childKey == "a2")
+          {
+            a2 = childValue;
+          }
+          else if (childKey == "a3")
+          {
+            a3 = childValue;
+          }
+
+        });
+
+
+        //$( "#articleList" ).append( '<li> <div class="collapsible-header">' + childKey + '</div> <div class="collapsible-body"><span>' + childData +  '</span></div> </li>');
+    $("#questionsList").append(html1 + q + html2 + a1 + html3 + a2 + html3 + a3 + html4);
+      });
+      $( "#questionsDiv" ).fadeIn(1000);
+    });
+  });
+
+}
+
+
+function addQuestion()
+{
+
+  if(theCategory == "")
+  {
+    Materialize.toast('Choose a category first!', 2000);
+    return;
+  }
+
+  if($( "#newQuestionDiv" ).css("display") == "none")
+  $( "#newQuestionDiv" ).fadeIn(1000);
+  else {
+    $( "#newQuestionDiv" ).fadeOut(1000);
+  }
+
+}
+
+
+function newQuestionAdd()
+{
+  var _question = $("#question").val();
+  var _a1 = $("#a1").val();
+  var _a2 = $("#a2").val();
+  var _a3 = $("#a3").val();
+  Materialize.toast('Adding...', 500);
+
+  var postData = {
+      question: _question,
+      a1: _a1,
+      a2: _a2,
+      a3: _a3
+    };
+
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('quizy/' + theCategory).push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/quizy/'+ theCategory + "/"+ newPostKey] = postData;
+
+    firebase.database().ref().update(updates);
+    $( "#newQuestionDiv" ).fadeOut(500);
+    chooseCategory(theCategory);
+
 
 }
