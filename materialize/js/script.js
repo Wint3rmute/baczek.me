4eb624e2cec2a2dc056a7c555d1ed829dc013882
Mateusz Baczek <mateusz.baczek1998@gmail.com>
2017-03-10 21:08:54 +0100

    working on auth

--- materialize/js/script.js
+++ materialize/js/script.js
@@ -1,7 +1,33 @@
-$(document).ready(function() { getCategories(); });
+$(document).ready(function() { login(); getCategories(); });
 
 
 var theCategory = "";
+
+function login()
+{
+  webSettings.setDomStorageEnabled(true);
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+
+  alert("success, " + user);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+  alert("failuer! " + errorCode);
+});
+
+}
 
 function getCategories()
 {
