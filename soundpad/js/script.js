2acd49171271db8361ab4a35f79a5e1abed1676b
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2017-03-30 19:25:06 +0300

    A E S T H E T CS

--- soundpad/js/script.js
+++ soundpad/js/script.js
@@ -1,81 +1,71 @@
-var frequency = [440,440,440,440];
-var type =  ['sine','sine','sine','sine'];
-var isPlaying = [false,false,false,false];
+var frequency = [440, 440, 440, 440];
+var type = ['sine', 'sine', 'sine', 'sine'];
+var isPlaying = [false, false, false, false];
 var Waves = [];
 
 $(document).ready(function() {
     $('select').material_select();
     $('pulse').material_select();
 
-    for(var i = 0; i < 4; i++)
-    {
-     Waves[i] = new Pizzicato.Sound({
-        source: 'wave',
-        options: {
-            frequency: frequency[0],
-            type: type[0]
-        }
-     });
-
-
+    for (var i = 0; i < 4; i++) {
+        Waves[i] = new Pizzicato.Sound({
+            source: 'wave',
+            options: {
+                frequency: frequency[0],
+                type: type[0]
+            }
+        });
     }
-alert("Ready!");
-  });
+});
 
 
 
 
 
 
- function refreshWave(whichOne)
- {
-     Waves[whichOne].stop();
+function refreshWave(whichOne) {
+    Waves[whichOne].stop();
 
-     Waves[whichOne] = new Pizzicato.Sound({
+    Waves[whichOne] = new Pizzicato.Sound({
         source: 'wave',
         options: {
             frequency: frequency[whichOne],
             type: type[whichOne]
         }
-     });
+    });
 
-     if(isPlaying[whichOne])
-       Waves[whichOne].play();
- }
-
-function changeF(whichOne, value)
-{
-//alert(value);
-  frequency[whichOne] = value;
-
-  refreshWave(whichOne);
+    if (isPlaying[whichOne])
+        Waves[whichOne].play();
 }
 
-function changeT(whichOne)
-{
-  var value =  $('#type' + whichOne).find(":selected").text().toLowerCase();
+function changeF(whichOne, value) {
+    //alert(value);
+    frequency[whichOne] = value;
 
-  type[whichOne] = value;
-  refreshWave(whichOne);
+    refreshWave(whichOne);
+}
+
+function changeT(whichOne) {
+    var value = $('#type' + whichOne).find(":selected").text().toLowerCase();
+
+    type[whichOne] = value;
+    refreshWave(whichOne);
 
 }
 
-function play(whichOne)
-{
-  refreshWave(whichOne);
-  if(isPlaying[whichOne])
-  {
-    Waves[whichOne].stop();
-    $("#play" + whichOne).html('<i class="material-icons">volume_off </i>');
-     $("#play" + whichOne).removeClass("pulse");
-    isPlaying[whichOne] = false;
-  }
-  else {
-    Waves[whichOne].play();
-    $("#play" + whichOne).html('<i class="material-icons">volume_up</i>');
-      $("#play" + whichOne).addClass("pulse");
-    isPlaying[whichOne] = true;
-  }
+function play(whichOne) {
+    refreshWave(whichOne);
+    if (isPlaying[whichOne]) {
+        Waves[whichOne].stop();
+        $("#play" + whichOne).html('<i class="material-icons">volume_off </i>');
+        $("#play" + whichOne).removeClass("pulse");
+        isPlaying[whichOne] = false;
+    } else {
+        Waves[whichOne].play();
+        $("#play" + whichOne).html('<i class="material-icons">volume_up</i>');
+        $("#play" + whichOne).addClass("pulse");
+        isPlaying[whichOne] = true;
+    }
 
-  //alert(whichOne + " " + frequency[whichOne] + " " + type[whichOne]);
+    //alert(whichOne + " " + frequency[whichOne] + " " + type[whichOne]);
 }
